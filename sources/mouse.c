aa0ab47fde3d6cc3e1d56e57a339bd2cc9d65cc4
faraz7704 <faraz7704@gmail.com>
2022-07-20 20:09:28 +0400

    done

--- sources/mouse.c
+++ sources/mouse.c
@@ -6,16 +6,16 @@
 /*   By: fkhan <fkhan@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 23:07:19 by fkhan             #+#    #+#             */
-/*   Updated: 2022/07/19 22:03:07 by fkhan            ###   ########.fr       */
+/*   Updated: 2022/07/20 19:09:44 by fkhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static void	mouse_scroll(int x, int y, t_event_data *data)
+static void	mouse_scroll(int x, int y, double new_zoom, t_event_data *data)
 {
 	static t_vector2	prev_pos;
-	static t_vector2	fix_pos;
+	t_vector2			fix_pos;
 	t_vector2			new_pos;
 	double				zoom;
 
@@ -24,11 +24,13 @@ static void	mouse_scroll(int x, int y, t_event_data *data)
 		prev_pos = init_vector2(x, y);
 	new_pos.x = (x - prev_pos.x);
 	new_pos.y = (y - prev_pos.y);
+	fix_pos = data->fractolinfo->zoom_offset;
 	if (fix_pos.x == 0 && fix_pos.y == 0)
 		fix_pos = init_vector2(x, y);
-	printf("new_pos x: %f, y: %f\n", new_pos.x, new_pos.y);
-	new_pos.x = fix_pos.x + ((new_pos.x / WIDTH) * WIDTH * zoom);
-	new_pos.y = fix_pos.y + ((new_pos.y / HEIGHT) * HEIGHT * zoom);
+	printf("diff world_x: %f, zoom_x: %f zoom: %f\n", new_pos.x, (((new_pos.x / WIDTH) * WIDTH * zoom)), zoom);
+	new_pos.x = fix_pos.x + (((new_pos.x / WIDTH) * WIDTH * zoom));
+	new_pos.y = fix_pos.y + (((new_pos.y / HEIGHT) * HEIGHT * zoom));
+	data->fractolinfo->zoom = new_zoom;
 	data->fractolinfo->zoom_offset = new_pos;
 	draw_fractol(data->appinfo, data->fractolinfo);
 	prev_pos.x = x;
@@ -37,15 +39,18 @@ static void	mouse_scroll(int x, int y, t_event_data *data)
 
 int	mouse_down_handler(int button, int x, int y, t_event_data *data)
 {
+	double	zoom;
+
+	zoom = data->fractolinfo->zoom;
 	if (button == ON_MOUSE_SCROLL_UP)
 	{
-		data->fractolinfo->zoom *= ZOOM_FACTOR;
-		mouse_scroll(x, y, data);
+		zoom *= ZOOM_FACTOR;
+		mouse_scroll(x, y, zoom, data);
 	}
 	else if (button == ON_MOUSE_SCROLL_DOWN)
 	{
-		data->fractolinfo->zoom /= ZOOM_FACTOR;
-		mouse_scroll(x, y, data);
+		zoom /= ZOOM_FACTOR;
+		mouse_scroll(x, y, zoom, data);
 	}
 	return (0);
 }
